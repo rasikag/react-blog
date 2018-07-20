c100a72d651040b426f86445879f58a6857e9304
rasikag <rasikagayan90@gmail.com>
2018-07-21 02:40:13 +0800

    Redirecting Requests

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -33,6 +33,8 @@ class Blog extends Component {
                 <Switch>
                     <Route path="/new-post" component={NewPost} />  
                     <Route path="/posts" component={Posts} />
+                    <Redirect to="/posts" from="/" />
+                    {/* <Route path="/" component={Posts} /> */}
                 </Switch>
             </div>
         );
