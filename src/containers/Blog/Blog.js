8e4fc0ef16dc07aba65d4ea050324554e96d2903
rasikag <rasikagayan90@gmail.com>
2018-07-20 22:27:03 +0800

    Switching Between Pages

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -24,8 +24,6 @@ class Blog extends Component {
                 </header>
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
-                {/* <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} /> */}
                 <Route path="/" exact component={Posts} />
                 <Route path="/new-post" component={NewPost} />
             </div>
